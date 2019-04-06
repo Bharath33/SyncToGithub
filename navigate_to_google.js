ff26851d2b4e854aff19da3f3aa0e1f68cea65a6
Bharath S <bharathgates33@gmail.com>
2019-04-06 20:31:02 +0530

    first commit

---
+++ navigate_to_google.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer')
+try {
+  (async () => {
+    const browser = await puppeteer.launch({ headless: false})
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1280, height: 800 })
+    await page.goto('https://www.google.com')
+    await page.screenshot({path: 'amazon.png'})
+    //await browser.close()
+    console.log('See screenshot')
+  })()
+} catch (err) {
+  console.error(err)
+}
